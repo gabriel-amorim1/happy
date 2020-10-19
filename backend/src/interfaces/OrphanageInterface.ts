85d956550ca6aef3933f99e6ae2bae39d77f2716
Gabriel Amorim <therealamorim@gmail.com>
2020-10-19 12:03:31 -0300

    Feat: Created OrphanageInterface

---
+++ backend/src/interfaces/OrphanageInterface.ts
@@ -0,0 +1,15 @@
+interface OrphanageInterface {
+    name: string;
+    latitude: number;
+    longitude: number;
+    about: string;
+    instructions: string;
+    opening_hours: string;
+    open_on_weekends: boolean;
+    images: Array<{
+        id: number;
+        path: string;
+    }>;
+
+    id?: number;
+}
