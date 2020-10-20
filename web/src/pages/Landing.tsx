7158ad8d0d523d334b3c58b7ccc9993ff95dd404
Gabriel Amorim <therealamorim@gmail.com>
2020-10-20 12:52:53 -0300

    Feat: Added restrict access button to landing screen

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -10,17 +10,25 @@ function Landing() {
     return (
         <div id="page-landing">
             <div className="content-wrapper">
-                <img src={logoImg} alt=""/>
+                <div className="logo-location">
+                    <img src={logoImg} alt=""/>
+
+                    <div className="location">
+                        <strong>Contagem</strong>
+                        <span>Minas Gerais</span>
+                    </div>
+                </div>
+
+                <Link to="">
+                    <button type="button" className="restrict-access">
+                        Acesso Restrito
+                    </button>
+                </Link>
 
                 <main>
                     <h1>Leve felicidade para o mundo</h1>
                     <p>Visite orfanatos e mude o dia de muitas crianças.</p>
                 </main>
-
-                <div className="location">
-                    <strong>Contagem</strong>
-                    <span>Minas Gerais</span>
-                </div>
 
                 <Link to="/app" className="enter-app">
                     <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
