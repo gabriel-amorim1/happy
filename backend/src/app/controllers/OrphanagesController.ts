490c796d8946bba4d26927ae7d1c66a1dafc1ed1
Gabriel Amorim <therealamorim@gmail.com>
2020-10-20 19:27:42 -0300

    Feat: Added Functions Return Type on OrphanagesController

--- backend/src/app/controllers/OrphanagesController.ts
+++ backend/src/app/controllers/OrphanagesController.ts
@@ -4,11 +4,15 @@ import orphanageView from '../views/orphanages_view';
 
 import * as model from '../models/orphanage';
 import { handleDataToCreateOrphanage } from '../utils/orphanage/handler';
+import {
+    CreateOrphanageRequestInterface,
+    OrphanageViewInterface,
+} from '../interfaces/OrphanageInterface';
 
 export const create = async (
     data: CreateOrphanageRequestInterface,
     files: Express.Multer.File[],
-): Promise<any> => {
+): Promise<OrphanageViewInterface> => {
     if (files.length === 0)
         throw new HttpError(400, 'files is a required field');
 
@@ -19,7 +23,7 @@ export const create = async (
     return orphanageView.render(orphanage);
 };
 
-export const getById = async (id: string): Promise<any> => {
+export const getById = async (id: string): Promise<OrphanageViewInterface> => {
     const orphanage = await model.getById(id);
 
     if (!orphanage) throw new HttpError(404, 'Orphanage not found');
@@ -27,7 +31,7 @@ export const getById = async (id: string): Promise<any> => {
     return orphanageView.render(orphanage);
 };
 
-export const getAll = async (): Promise<any> => {
+export const getAll = async (): Promise<OrphanageViewInterface[]> => {
     const orphanages = await model.getAll();
 
     return orphanageView.renderMany(orphanages);
